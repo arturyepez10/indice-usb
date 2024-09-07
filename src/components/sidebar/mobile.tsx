7f731e054653529043f6a7262d91d2919d75972d
Arturo Yepez <arturyepez10@gmail.com>
2024-09-06 22:09:06 -0400

    feat: added mobile component for sidebar as drawer

---
+++ src/components/sidebar/mobile.tsx
@@ -0,0 +1,75 @@
+import { useState } from 'react';
+
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+
+import CloseIcon from '@mui/icons-material/Close';
+import ExpandMoreRoundedIcon from '@mui/icons-material/ExpandMoreRounded';
+
+import Info from './info';
+
+export const MobileSidebar = () => {
+  const [open, setOpen] = useState(false);
+
+  const toggleDrawer = (state: boolean) => () => {
+    setOpen(state);
+  };
+
+  const DrawerList = (
+    <Box sx={{ width: 'auto', px: 3, pb: 3, pt: 8 }} role="presentation">
+      <IconButton
+        onClick={toggleDrawer(false)}
+        sx={{ position: 'absolute', right: 8, top: 8 }}
+      >
+        <CloseIcon />
+      </IconButton>
+      <Info />
+    </Box>
+  );
+
+  return (
+    <Card
+      sx={{
+        display: { xs: 'flex', md: 'none' },
+        width: '100%',
+        flexDirection: 'column',
+        maxWidth: '100%',
+        backgroundColor: { xs: 'transparent', sm: 'background.default' },
+        alignItems: 'center',
+        mt: { xs: 2, sm: 4 },
+        mb: { xs: 0, sm: 4 },
+        mx: { xs: 2, sm: 10 },
+        gap: { xs: 4, md: 2 },
+      }}
+    >
+      <CardContent
+        sx={{
+          display: 'flex',
+          width: '100%',
+          alignItems: 'center',
+          justifyContent: 'center',
+          ':last-child': { pb: 2 },
+        }}
+      >
+        <div>
+          <Button
+            variant="text"
+            endIcon={<ExpandMoreRoundedIcon />}
+            onClick={toggleDrawer(true)}
+          >
+            Ver detalles
+          </Button>
+          <Drawer open={open} anchor="top" onClose={toggleDrawer(false)}>
+            {DrawerList}
+          </Drawer>
+        </div>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default MobileSidebar;
---
+++ src/components/sidebar/web.tsx
@@ -0,0 +1,52 @@
+import { Grid, Box } from '@mui/material';
+
+import Info from './info';
+import ToggleColorMode from "../ToggleColorMode";
+import FormDialog from '../form-dialog';
+
+export const WebSidebar = () => {
+
+  return (
+    <Grid
+      item
+      xs={12}
+      sm={5}
+      lg={4}
+      sx={{
+        display: { xs: 'none', md: 'flex' },
+        flexDirection: 'column',
+        backgroundColor: 'background.paper',
+        borderRight: { sm: 'none', md: '1px solid' },
+        borderColor: { sm: 'none', md: 'divider' },
+        alignItems: 'start',
+        pt: 4,
+        px: 10,
+        gap: 4,
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          width: '100%'
+        }}
+      >
+        <ToggleColorMode />
+        <FormDialog />
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          flexGrow: 1,
+          width: '100%',
+          maxWidth: 500,
+        }}
+      >
+        <Info />
+      </Box>
+    </Grid>
+  )
+}
+
+export default WebSidebar;
