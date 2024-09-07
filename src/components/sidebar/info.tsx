91a97a70cc32f4bfe8a40f95d2f72530d4b8dfcb
Arturo Yepez <arturyepez10@gmail.com>
2024-09-06 21:32:26 -0400

    feat: full refactor of components

--- src/components/sidebar/info.tsx
+++ src/components/sidebar/info.tsx
@@ -1,60 +1,96 @@
-import * as React from 'react';
+import { useState } from 'react';
 
+import Box from '@mui/material/Box';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import Typography from '@mui/material/Typography';
 
-const products = [
+import useAcademicSummary from '../../use/use-summary';
+
+const statistics = [
   {
-    name: 'Professional plan',
-    desc: 'Monthly subscription',
-    price: '$15.00',
+    name: "Inscritos",
+    desc: "",
+    properties: ["total_courses", "total_credits"]
   },
   {
-    name: 'Dedicated support',
-    desc: 'Included in the Professional plan',
-    price: 'Free',
+    name: "Aprobados",
+    desc: "",
+    properties: ["total_courses_passed", "total_credits_passed"]
   },
   {
-    name: 'Hardware',
-    desc: 'Devices needed for development',
-    price: '$69.99',
+    name: "Retirados",
+    desc: "",
+    properties: ["total_courses_removed", "total_credits_removed"]
   },
   {
-    name: 'Landing page template',
-    desc: 'License',
-    price: '$49.99',
-  },
+    name: "Reprobados",
+    desc: "",
+    properties: ["total_courses_failed", "total_credits_failed"]
+  }
 ];
 
-// interface InfoProps { }
+type PropertiesType = 
+  "total_courses"
+  | "total_credits"
+  | "total_courses_passed"
+  | "total_credits_passed"
+  | "total_courses_removed"
+  | "total_credits_removed"
+  | "total_courses_failed"
+  | "total_credits_failed";
 
-export default function Info(
-  
-) {
+export default function Info() {
+  const { summary } = useAcademicSummary();
+
+  const [tab, setTab] = useState(0);
+
+  const handleTabChange = (_: React.SyntheticEvent, newValue: number) => {
+    setTab(newValue);
+  }
+
   return (
-    <React.Fragment>
+    <>
       <Typography variant="subtitle2" color="text.secondary">
-        Indice Académico
+        INDICE ACADÉMICO
       </Typography>
       <Typography variant="h4" gutterBottom>
-        {'$144.97'}
+        {summary.summary_grade()}
       </Typography>
-      <List disablePadding>
-        {products.map((product) => (
-          <ListItem key={product.name} sx={{ py: 1, px: 0 }}>
+
+      <Box width="100%">
+        <Tabs value={tab} onChange={handleTabChange} variant="fullWidth" centered>
+          {
+            ["Asignaturas", "Créditos"].map((label, index) => (
+              <Tab
+                key={`index-${index}`}
+                label={
+                  <Typography variant="body1" color="text.secondary" textTransform="capitalize">
+                    {label}
+                  </Typography>
+                }
+              />
+            ))
+          }
+        </Tabs>
+      </Box>
+      <List>
+        {statistics.map((info) => (
+          <ListItem key={info.name} sx={{ py: 1, px: 0 }}>
             <ListItemText
               sx={{ mr: 2 }}
-              primary={product.name}
-              secondary={product.desc}
+              primary={info.name}
+              secondary={info.desc}
             />
             <Typography variant="body1" fontWeight="medium">
-              {product.price}
+              {summary[info.properties[tab] as PropertiesType]}
             </Typography>
           </ListItem>
         ))}
       </List>
-    </React.Fragment>
+    </>
   );
 }
