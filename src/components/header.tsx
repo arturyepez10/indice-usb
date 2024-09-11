c7b2ad23f2230dd020692723d81e33053f4b6c5d
Arturo Yepez <arturyepez10@gmail.com>
2024-09-10 21:47:13 -0400

    fix: remove toggle color mode in mobile since clashes in mobile view

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,4 @@
 import Box from "@mui/material/Box"
-import ToggleColorMode from "./ToggleColorMode"
 import Typography from "@mui/material/Typography"
 
 export const Header = () => {
@@ -7,23 +6,14 @@ export const Header = () => {
     <Box
       sx={{
         display: 'flex',
-        justifyContent: { sm: 'space-between', md: 'center' },
+        justifyContent: 'center',
         alignItems: 'center',
         width: '100%',
         maxWidth: { sm: '100%' },
       }}
     >
       {/* Mobile view: Toggle color mode */}
-      <Box
-        sx={{
-          display: { sm: 'flex', md: 'none' },
-          flexDirection: 'row',
-          width: '100%',
-          justifyContent: 'space-between',
-        }}
-      >
-        <ToggleColorMode />
-      </Box>
+      {/* To be added in the future somewhere the designs allow it */}
 
       {/* Main Title */}
       <Typography variant="h4" minWidth="fit-content">Informe Academico</Typography>
